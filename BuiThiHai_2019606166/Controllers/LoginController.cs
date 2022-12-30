031b526105386b98c78bac5b34b721c99f649001
Winkyy2k1 <Thanhhai.face@com>
2022-12-30 10:57:43 +0700

    3012

---
+++ BuiThiHai_2019606166/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using BuiThiHai_2019606166.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BuiThiHai_2019606166.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+
+        private Model1 db = new Model1();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            var us = db.tblUsers.Where(u => u.username == username && u.password == password).FirstOrDefault();
+            if (us == null)
+            {
+                ViewBag.errLogin = " không thể đanwg nhập";
+                return View("Login");
+            }
+            else
+            {
+                Session["username"] = username;
+                return RedirectToAction("Index", "NhanViens");
+            }    
+        }
+        public ActionResult Logout()
+        {
+            Session["username"] = null;
+            return RedirectToAction("Index", "NhanViens");
+        }
+    }
+}
---
+++ BuiThiHai_2019606166/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BuiThiHai_2019606166.MvcApplication" Language="C#" %>
